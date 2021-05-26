010773721cbd80dab80671423cbb1d8a4d63e6cd
Biola <52200708+decorouz@users.noreply.github.com>
2021-05-27 00:42:16 +0100

    Delete moved files

--- week2/csv_reader.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('addresses.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        firstname, lastname, street, city, state, zipcode = row
-        print("Name: {} {}, Address: {}, city: {}, state: {}, zipcode: {}".format(
-            firstname, lastname, street, city, state, zipcode))
