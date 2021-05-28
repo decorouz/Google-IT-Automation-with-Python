33888c82446e7f7d31cfc15b1bce30856e177b3f
Biola <52200708+decorouz@users.noreply.github.com>
2021-05-28 04:28:34 +0100

    Add check_disk func to main func and rename parameters

--- intro_git_github/all_checks.py
+++ intro_git_github/all_checks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/ python3
 import os
 import sys
+import shutil
 
 
 def check_reboot():
@@ -9,11 +10,29 @@ def check_reboot():
     return os.path.exists("/run/reboot-required")
 
 
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there is enough free disk and false if otherwise
+    """
+
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return False
+    return True
+
+
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    print("Everything is ok!")
+    if check_disk_full("/", min_gb=2, min_percent=10):
+        print("Disk full.")
+        sys.exit(1)
+    print("Everything ok.")
     sys.exit(0)
 
 
